4888180193eaf7d6eb5a00c21ccf048e90c71945
Batya Zamansky <batya.zamansky@gmail.com>
2014-10-23 17:10:57 -0400

    working more on figuring out how to get the appropriate data from the api

--- app.py
+++ app.py
@@ -10,12 +10,13 @@ def home():
 	url = ""
 	varl = {}
 	if request.method == "POST":
+		term = request.form['term']
 		varl['subj'] = request.form['subject']
 		varl['level'] = request.form['level']
-		url = getClasses("SP15",varl)
+		url = getClasses(term,varl)
 
 
 	return render_template("index.html",url=url)
 
 if __name__ == '__main__':
-    app.run(debug = True, host="0.0.0.0", port = 7999)
+    app.run(debug = True, host="0.0.0.0", port = 5000)
--- roster.py
+++ roster.py
@@ -7,8 +7,34 @@ API details: https://classes.cornell.edu/content/SP15/api-details
 
 
 def getClasses(term,varl):
+	'''
+	vars:
+	{subj:'subject'
+	,level:'level'}
+
+	From each class, want:
+	(the ['classes'][0] will need to loop through all relevant classes, 
+		possibly only show so many on a page)
+	(sections will need to be properly parsed)
+
+	{'classId':data['data']['classes'][0]['catalogNbr']
+	,'title':data['data']['classes'][0]['titleLong']
+	,'description':data['data']['classes'][0]['description']
+	,'semester':data['data']['classes'][0]['catalogWhenOffered']
+	,'forbiddenOver':data['data']['classes'][0]['catalogForbiddenOverlaps']
+	,'prereq':data['data']['classes'][0]['catalogPrereqCoreq']
+	,'sections':data['data']['classes'][0]['classSections']}
+	'''
 	url = "https://classes.cornell.edu/api/2.0/search/classes.json?roster=%s&subject=%s&classLevels[]=%s"%(term,varl['subj'],varl['level'])
 	data = json.loads(urllib2.urlopen(url).read())
+	output = {'classId':data['data']['classes'][0]['catalogNbr']
+	,'title':data['data']['classes'][0]['titleLong']
+	,'description':data['data']['classes'][0]['description']
+	,'semester':data['data']['classes'][0]['catalogWhenOffered']
+	,'forbiddenOver':data['data']['classes'][0]['catalogForbiddenOverlaps']
+	,'prereq':data['data']['classes'][0]['catalogPrereqCoreq']
+	,'sections':len(data['data']['classes'][0]['enrollGroups'][0]['classSections'])}
+
 	classes = data['data']['classes'][0]['enrollGroups'][0]['classSections'][0]
-	return classes
+	return output
 
