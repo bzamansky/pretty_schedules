83fc163931bad257862c0934fb1d47debbea884f
Batya Zamansky <batya.zamansky@gmail.com>
2014-11-03 20:50:27 -0500

    commiting updates

--- app.py
+++ app.py
@@ -7,16 +7,19 @@ app.secret_key="blah"
 
 @app.route("/", methods = ["GET","POST"])
 def home():
-	url = ""
 	varl = {}
+	data = {}
 	if request.method == "POST":
 		term = request.form['term']
 		varl['subj'] = request.form['subject']
 		varl['level'] = request.form['level']
-		url = getClasses(term,varl)
+		data = getClasses(term,varl)
+		print data[0].keys()
 
 
-	return render_template("index.html",url=url)
+
+
+	return render_template("index.html",classes=data)
 
 if __name__ == '__main__':
     app.run(debug = True, host="0.0.0.0", port = 5000)
--- roster.py
+++ roster.py
@@ -25,16 +25,25 @@ def getClasses(term,varl):
 	,'prereq':data['data']['classes'][0]['catalogPrereqCoreq']
 	,'sections':data['data']['classes'][0]['classSections']}
 	'''
+
+	
 	url = "https://classes.cornell.edu/api/2.0/search/classes.json?roster=%s&subject=%s&classLevels[]=%s"%(term,varl['subj'],varl['level'])
 	data = json.loads(urllib2.urlopen(url).read())
+	'''
+	This gets the sections for an individual class
 	output = {'classId':data['data']['classes'][0]['catalogNbr']
+
 	,'title':data['data']['classes'][0]['titleLong']
 	,'description':data['data']['classes'][0]['description']
 	,'semester':data['data']['classes'][0]['catalogWhenOffered']
 	,'forbiddenOver':data['data']['classes'][0]['catalogForbiddenOverlaps']
 	,'prereq':data['data']['classes'][0]['catalogPrereqCoreq']
-	,'sections':len(data['data']['classes'][0]['enrollGroups'][0]['classSections'])}
+	,'sections':data['data']['classes'][0]['enrollGroups'][0]['classSections']}
 
 	classes = data['data']['classes'][0]['enrollGroups'][0]['classSections'][0]
+	'''
+
+	output = data['data']['classes']
+
 	return output
 
